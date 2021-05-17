e01fe86ff059e7c1bf3e96046547406bfd0b6c0e
rgg7888 <rgg7888@outlook.com>
2021-05-17 14:24:41 -0600

    updating

--- index.php
+++ index.php
@@ -2,3 +2,11 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
+page([
+    'idioma' => 'en',
+    'head' => [],
+    'body' => [
+        'contenido' => [],
+        'atributos' => []
+    ]
+]);
