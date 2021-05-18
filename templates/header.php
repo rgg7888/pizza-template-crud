88493f1fff2fa7cfcf89d5e01019d96f8339d6ce
rgg7888 <rgg7888@outlook.com>
2021-05-17 22:03:30 -0600

    updating

--- templates/header.php
+++ templates/header.php
@@ -4,10 +4,21 @@ $barra_de_navegacion = implode("",[
     nav([
         _div([
             'class' => 'container'
-        ],a("Pizza",[
-            'href' => '#',
-            'class' => 'brand-logo brand-text'
-        ]))
+        ],[
+            a("Pizza",[
+                'href' => '#',
+                'class' => 'brand-logo brand-text'
+            ]),
+            _ul([
+                'id' => 'nav-mobile',
+                'class' => 'right hide-on-small-and-down'
+            ],[
+                li(_a([
+                    'href' => '#',
+                    'class' => 'btn brand'
+                ],"text"))
+            ])
+        ])
     ],[
         'class' => 'white z-depth-0'
     ])
