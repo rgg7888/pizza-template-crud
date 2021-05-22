b76a3f5e045b3bf8896ae450db556845cb6e8d64
rgg7888 <rgg7888@outlook.com>
2021-05-22 17:22:14 -0600

    separando la logica de la vista

--- add.php
+++ add.php
@@ -5,29 +5,7 @@ require_once('./templates/header.php');
 require_once('./templates/footer.php');
 
 #######Procesamiento de los datos###########
-if($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $result = validar_form([
-        'email',
-        'title',
-        'ingredients'
-    ]);
-    if(!empty($result)){
-        echo $result[0]."<br>";
-        echo $result[1]."<br>";
-        echo $result[2];
-    }
-}else{
-    $result = _validar_form([
-        'email',
-        'title',
-        'ingredients'
-    ]);
-    if(!empty($result)){
-        echo $result[0]."<br>";
-        echo $result[1]."<br>";
-        echo $result[2];
-    }
-}
+require_once('./data/addData.php');
 ###########################################
 
 page([
