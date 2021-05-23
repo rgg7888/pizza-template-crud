4528df48ed680f382fe3162cb4407cf3a38b54e3
rgg7888 <rgg7888@outlook.com>
2021-05-23 02:40:11 -0600

    updating

--- controladores/validaController.php
+++ controladores/validaController.php
@@ -17,16 +17,16 @@ if(isset($_POST['submit'])) {
     #comprobamos que el email no este vacio
     (is_array(validar("vacio","email"))) ? $errores['email'] = validar("vacio","email")[1] : $values['email'] = validar("vacio","email");
     #comprobamos que el email sea valido
-    (is_array(validar("correo","email"))) ? $errores['email'] = validar("correo","email")[1] : $values['email'] = validar("correo","email");
+    (is_array(validar("correo","email"))) ? $errores['email'] .= "<br>".validar("correo","email")[1] : $values['email'] = validar("correo","email");
 
     #comprobamos que el title no este vacio
     (is_array(validar("vacio","title"))) ? $errores['title'] = validar("vacio","title")[1] : $values['title'] = validar("vacio","title");
     #comprobamos que el title sea valido
-    (is_array(validar("letrasOnly","title"))) ? $errores['title'] = validar("letrasOnly","title")[1] : $values['title'] = validar("letrasOnly","title");
+    (is_array(validar("letrasOnly","title"))) ? $errores['title'] .= "<br>".validar("letrasOnly","title")[1] : $values['title'] = validar("letrasOnly","title");
 
     #comprobamos que los ingredientes no este vacio
     (is_array(validar("vacio","ingredients"))) ? $errores['ingredients'] = validar("vacio","ingredients")[1] : $values['ingredients'] = validar("vacio","ingredients");
     #comprobamos que el email sea valido
-    (is_array(validar("lista","ingredients"))) ? $errores['ingredients'] = validar("lista","ingredients")[1] : $values['ingredients'] = validar("lista","ingredients");
+    (is_array(validar("lista","ingredients"))) ? $errores['ingredients'] .= "<br>".validar("lista","ingredients")[1] : $values['ingredients'] = validar("lista","ingredients");
 
 }
