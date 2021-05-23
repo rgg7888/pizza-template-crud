dd851c4599d48d74b862c47c1b91467401eca9bd
rgg7888 <rgg7888@outlook.com>
2021-05-23 15:13:13 -0600

    implementacion de la funcion delete

--- controladores/getOneRowController.php
+++ controladores/getOneRowController.php
@@ -1,5 +1,8 @@
 <?php
 $row = null;
+if(isset($_POST['delete'])) {
+    delete($conn,"id_to_delete","Pizza");
+}
 if(isset($_GET['id'])) {
     $row = byId($conn,"Pizza");
 }
--- templates/single.php
+++ templates/single.php
@@ -6,7 +6,23 @@ if($row) {
         p("Created by: ".htmlspecialchars($row['email'])),
         p(date($row['created_at'])),
         h5("Ingredients:"),
-        p(htmlspecialchars($row['ingredients']))
+        p(htmlspecialchars($row['ingredients'])),
+        form([
+            input([
+                'type' => 'hidden',
+                'name' => 'id_to_delete',
+                'value' => $row['id']
+            ]),
+            input([
+                'type' => 'submit',
+                'name' => 'delete',
+                'value' => 'Delete',
+                'class' => 'btn brand z-depth-0'
+            ])
+            ],[
+                'action' => 'http://localhost:8002',
+                'method' => 'POST'
+            ])
     ];
 }else{
     $singleTemplate = h5("404 Not Found");
